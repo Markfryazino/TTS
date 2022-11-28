6b775b0750f669dbe24296ae399b8b4fd6919b38
Mark Rofin <broccoliman@qavm-efb872e7.qemu>
2022-11-28 18:49:51 +0300

    rm trash

--- train.py
+++
@@ -1,9 +0,0 @@
-from src.configs import TrainConfig, FastSpeech2Config, MelSpectrogramConfig
-from src.training.training_loop import train, prepare
-
-model_config = FastSpeech2Config()
-train_config = TrainConfig()
-mel_config = MelSpectrogramConfig()
-
-model, train_loader, fastspeech_loss, optimizer, scheduler, wave_glow = prepare(model_config, train_config, mel_config)
-train(train_config, model, train_loader, fastspeech_loss, optimizer, scheduler, wave_glow)
